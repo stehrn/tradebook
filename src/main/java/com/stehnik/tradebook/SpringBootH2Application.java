d5e999fb47cbc4673570a77c2802bfee59d22ba3
stehrn <nstehr_bus@hotmail.com>
2019-08-28 23:29:48 +0100

    Add H2 springboot to play around with queries

---
+++ src/main/java/com/stehnik/tradebook/SpringBootH2Application.java
@@ -0,0 +1,15 @@
+package com.stehnik.tradebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by nikstehr on 28/08/2019.
+ */
+@SpringBootApplication
+public class SpringBootH2Application {
+
+    public static void main(String... args) {
+        SpringApplication.run(SpringBootH2Application.class, args);
+    }
+}
